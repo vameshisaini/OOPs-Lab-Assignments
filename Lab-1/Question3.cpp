f556e371d04b5dee28eb18dc53b282299982bf43
vameshisaini <vameshi.saini2105@gmail.com>
2026-05-03 02:17:04 +0530

    Create Question3.cpp

---
+++ Lab-1/Question3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "\nAddition: " << a + b;
+    cout << "\nSubtraction: " << a - b;
+    cout << "\nMultiplication: " << a * b;
+    cout << "\nDivision: " << a / b;
+    return 0;
+}
