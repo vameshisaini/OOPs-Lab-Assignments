5235b13da015ac0ace2a068d6064f69f20f3d9fc
vameshisaini <vameshi.saini2105@gmail.com>
2026-05-03 02:16:40 +0530

    Create Question2.cpp

---
+++ Lab-1/Question2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    char c;
+    cout << "Enter an integer: ";
+    cin >> a;
+    cout << "Enter a decimal number: ";
+    cin >> b;
+    cout << "Enter a character: ";
+    cin >> c;
+    cout << "\nInteger: " << a;
+    cout << "\nDecimal: " << b;
+    cout << "\nCharacter: " << c;
+    return 0;
+}
