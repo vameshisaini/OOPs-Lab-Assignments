5e03e933d8e9a473b6c149df691f49c371c3edf3
vameshisaini <vameshi.saini2105@gmail.com>
2026-05-03 02:46:57 +0530

    Update ques3.cpp

--- Lab-9/ques3.cpp
+++ Lab-9/ques3.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-class Student {
-public:
-    int id;
-    char name[50];
-    void input() {
-        cout << "Enter ID: ";
-        cin >> id;
-        cout << "Enter Name: ";
-        cin >> name;
-    }
-    void display() {
-        cout << "ID: " << id << ", Name: " << name << endl;
-    }
-};
 int main() {
-    Student s;
-    s.input();
-    ofstream fout("student.dat", ios::binary);
-    fout.write((char*)&s, sizeof(s));
-    fout.close();
-    Student s2;
-    ifstream fin("student.dat", ios::binary);
-    fin.read((char*)&s2, sizeof(s2));
-    fin.close();
-    cout << "\nData read from file:\n";
-    s2.display();
+    ifstream fin("source.txt");
+    if (!fin) {
+        cout << "Source file not found!";
+        return 1;
+    }
+    ofstream fout("destination.txt");
+    if (!fout) {
+        cout << "Destination file cannot be created!";
+        return 1;
+    }
+    char ch;
+    while (fin.get(ch)) {
+        fout.put(ch);
+    }
+    cout << "File copied successfully!";
     return 0;
 }
