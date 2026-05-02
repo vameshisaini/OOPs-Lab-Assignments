36de0b2439c949fbc8e125c4a36d10d8f31de588
vameshisaini <vameshi.saini2105@gmail.com>
2026-05-03 02:49:38 +0530

    Update ques5.cpp

--- Lab-9/ques5.cpp
+++ Lab-9/ques5.cpp
@@ -1,32 +1,48 @@
-#include <iostream>
-#include <fstream>
+#include<iostream>
+#include<fstream>
 using namespace std;
-int main() {
-    char str[200];
-    cout << "Enter a string: ";
-    cin.getline(str, 200);
-    int length = 0;
-    while (str[length] != '\0') {
-        length++;
-    }
-    cout << "Length of string: " << length << endl;
-    ofstream fout("data.txt");
-    if (!fout) {
-        cout << "Error creating file!";
-        return 1;
-    }
-    fout << str;
+
+int main()
+{
+    ofstream fout("alpha.txt");
+    for(char ch='A';ch<='Z';ch++)
+        fout<<ch;
     fout.close();
-    ifstream fin("data.txt");
-    if (!fin) {
-        cout << "Error opening file!";
-        return 1;
-    }
+
+    fstream file("alpha.txt",ios::in|ios::out);
     char ch;
-    cout << "\nContent read from file:\n";
-    while (fin.get(ch)) {
-        cout << ch;
-    }
+
+    file.seekg(9);
+    file.get(ch);
+    cout<<"10th character: "<<ch<<endl;
+
+    file.seekp(4);
+    file.put('X');
+
+    file.seekg(0,ios::end);
+    cout<<"File size: "<<file.tellg()<<" bytes"<<endl;
+
+    file.seekg(-1,ios::end);
+    file.get(ch);
+    cout<<"Last character: "<<ch<<endl;
+
+    file.close();
+
+    ofstream fout2("data.txt");
+    fout2<<"Hello World\n";
+    fout2<<"This is a test file\n";
+    fout2<<"C++ File Handling";
+    fout2.close();
+
+    ifstream fin("data.txt");
+    fin.seekg(9);
+
+    cout<<"\nCurrent position: "<<fin.tellg()<<endl;
+    cout<<"Remaining content:\n";
+
+    while(fin.get(ch))
+        cout<<ch;
+
     fin.close();
     return 0;
 }
